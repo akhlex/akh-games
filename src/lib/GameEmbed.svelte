d4fbb601c353bcd89ebffb58c6d6f42d7a92849a
Akhil Alex <akhilalex003z@gmail.com>
2025-04-12 12:31:39 +0530

    test game embed with mari

---
+++ src/lib/GameEmbed.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let path = '';
+    export let title = '';
+</script>
+
+<iframe
+    src={path}
+    width="800"
+    height="600"
+    title={title}
+    style="border: none;">
+</iframe>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import GameEmbed from "$lib/GameEmbed.svelte";
+</script>
+
+<h1>Akh Games</h1>
+<!-- <GameEmbed path="/games/mari/index.html" title="Mari"/> -->
